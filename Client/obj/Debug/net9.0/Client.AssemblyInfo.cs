8cd2ff0544b887d7e7326be4391b78a655aef98a
Kirill Drinevskiy <drinevskiy3@gmail.com>
2024-12-05 15:25:02 +0300

    Initial client implementation

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1767c87ac946abfa4e97d25382446b7c09096e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
