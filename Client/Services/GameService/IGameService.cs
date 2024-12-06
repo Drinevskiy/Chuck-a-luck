a823a00079b77c6f322735a5199ea2d9c66c01f7
Kirill Drinevskiy <drinevskiy3@gmail.com>
2024-12-06 11:34:46 +0300

    Create domain project. Initial GameService.

--- Client/Services/GameService/IGameService.cs
+++ Client/Services/GameService/IGameService.cs
@@ -1,4 +1,6 @@
-﻿namespace Client.Services
+﻿using Domain.Models;
+
+namespace Client.Services
 {
     public interface IGameService
     {
@@ -12,7 +14,7 @@
         public Task<bool> StartGame(string game, string username, string field);
         public Task<string> GetOpponentField(string game, string username);
         public Task Move(string game, string username, int x, int y, bool shot);
-        public Task<string> GetContent(string username);
+        public Task<Player> GetUserInfo(string username);
         public Task AddContent(string username, string content);
         public Task<string> GetMove(string game);
         public Task AddMove(string game, string username);
